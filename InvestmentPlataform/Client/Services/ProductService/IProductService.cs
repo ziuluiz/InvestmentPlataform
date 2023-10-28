9c3998fbee44d9c87458cd763469474eb13515d6
ziuluiz <luiz_henrique_silva@outlook.com>
2023-10-27 21:22:42 -0300

    inicio construção categorias

---
+++ InvestmentPlataform/Client/Services/ProductService/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using InvestmentPlataform.Shared;
+using System.Threading.Tasks;
+
+namespace InvestmentPlataform.Client.Services.ProductService
+{
+	public interface IProductService
+	{
+		List<Product> Products { get; set; }
+		Task GetProducts();
+		Task<ServiceResponse<Product>> GetProduct(int productId);
+	}
+}
---
+++ InvestmentPlataform/Server/Services/ProductsServices/IProductService.cs
@@ -0,0 +1,8 @@
+﻿namespace InvestmentPlataform.Server.Services.ProductsServices
+{
+	public interface IProductService
+	{
+		Task<ServiceResponse<List<Product>>> GetProductsAsync();
+		Task<ServiceResponse<Product>> GetProductAsync(int productId);
+	}
+}
