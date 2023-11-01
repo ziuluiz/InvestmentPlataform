94f45cc2a18ccac7b1b8dea522c0fed48dfba9ea
ziuluiz <luiz_henrique_silva@outlook.com>
2023-11-01 09:22:54 -0300

    missing a I

--- InvestmentPlataform/Server/Controllers/OrderController.cs
+++ InvestmentPlataform/Server/Controllers/OrderController.cs
@@ -25,5 +25,12 @@ namespace InvestmentPlataform.Server.Controllers
 			var result = await _orderService.GetOrders();
 			return Ok(result);
 		}
-    }
+		[HttpGet("{orderId}")]
+		public async Task<ActionResult<ServiceResponse<OrderDetailsResponse>>> GetOrdersDetails(int orderId)
+		{
+			var result = await _orderService.GetOrderDetails(orderId);
+			return Ok(result);
+		}
+
+	}
 }
--- InvestmentPlataform/Server/Services/AuthService/AuthService.cs
+++ InvestmentPlataform/Server/Services/AuthService/AuthService.cs
@@ -6,140 +6,140 @@ using System.Security.Cryptography;
 namespace InvestmentPlataform.Server.Services.AuthService
 {
     public class AuthService : IAuthService
-	{
-		private readonly DataContext _context;
-		private readonly IConfiguration _configuration;
-		private readonly HttpContextAccessor _httpContextAccessor;
+    {
+        private readonly DataContext _context;
+        private readonly IConfiguration _configuration;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-		public AuthService(DataContext context,
-			IConfiguration configuration,
-			HttpContextAccessor httpContextAccessor)
-		{
-			_context = context;
-			_configuration = configuration;
-			_httpContextAccessor = httpContextAccessor;
-		}
+        public AuthService(DataContext context,
+            IConfiguration configuration,
+            IHttpContextAccessor httpContextAccessor)
+        {
+            _context = context;
+            _configuration = configuration;
+            _httpContextAccessor = httpContextAccessor;
+        }
 
-		public int GetUserId() => int.Parse(_httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier));
+        public int GetUserId() => int.Parse(_httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier));
 
-		public async Task<ServiceResponse<string>> Login(string email, string password)
-		{
-			var response = new ServiceResponse<string>();
-			var user = await _context.Users
-				.FirstOrDefaultAsync(x => x.Email.ToLower().Equals(email.ToLower()));
-			if (user == null)
-			{
-				response.Success = false;
-				response.Message = "User not found.";
-			}
-			else if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
-			{
-				response.Success = false;
-				response.Message = "Wrong password.";
-			}
-			else
-			{
-				response.Data = CreateToken(user);
-			}
+        public async Task<ServiceResponse<string>> Login(string email, string password)
+        {
+            var response = new ServiceResponse<string>();
+            var user = await _context.Users
+                .FirstOrDefaultAsync(x => x.Email.ToLower().Equals(email.ToLower()));
+            if (user == null)
+            {
+                response.Success = false;
+                response.Message = "User not found.";
+            }
+            else if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+            {
+                response.Success = false;
+                response.Message = "Wrong password.";
+            }
+            else
+            {
+                response.Data = CreateToken(user);
+            }
 
-			return response;
-		}
+            return response;
+        }
 
-		public async Task<ServiceResponse<int>> Register(User user, string password)
-		{
-			if (await UserExists(user.Email))
-			{
-				return new ServiceResponse<int>
-				{
-					Success = false,
-					Message = "User already exists."
-				};
-			}
+        public async Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            if (await UserExists(user.Email))
+            {
+                return new ServiceResponse<int>
+                {
+                    Success = false,
+                    Message = "User already exists."
+                };
+            }
 
-			CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
 
-			user.PasswordHash = passwordHash;
-			user.PasswordSalt = passwordSalt;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
 
-			_context.Users.Add(user);
-			await _context.SaveChangesAsync();
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
 
-			return new ServiceResponse<int> { Data = user.Id, Message = "Registration successful!" };
-		}
+            return new ServiceResponse<int> { Data = user.Id, Message = "Registration successful!" };
+        }
 
-		public async Task<bool> UserExists(string email)
-		{
-			if (await _context.Users.AnyAsync(user => user.Email.ToLower()
-				 .Equals(email.ToLower())))
-			{
-				return true;
-			}
-			return false;
-		}
+        public async Task<bool> UserExists(string email)
+        {
+            if (await _context.Users.AnyAsync(user => user.Email.ToLower()
+                 .Equals(email.ToLower())))
+            {
+                return true;
+            }
+            return false;
+        }
 
-		private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
-		{
-			using (var hmac = new HMACSHA512())
-			{
-				passwordSalt = hmac.Key;
-				passwordHash = hmac
-					.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-			}
-		}
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac
+                    .ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
 
-		private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
-		{
-			using (var hmac = new HMACSHA512(passwordSalt))
-			{
-				var computedHash =
-					hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-				return computedHash.SequenceEqual(passwordHash);
-			}
-		}
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash =
+                    hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
 
-		private string CreateToken(User user)
-		{
-			List<Claim> claims = new List<Claim>
-			{
-				new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-				new Claim(ClaimTypes.Name, user.Email)
-			};
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Email)
+            };
 
-			var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8
-				.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8
+                .GetBytes(_configuration.GetSection("AppSettings:Token").Value));
 
-			var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
-			var token = new JwtSecurityToken(
-					claims: claims,
-					expires: DateTime.Now.AddDays(1),
-					signingCredentials: creds);
+            var token = new JwtSecurityToken(
+                    claims: claims,
+                    expires: DateTime.Now.AddDays(1),
+                    signingCredentials: creds);
 
-			var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
 
-			return jwt;
-		}
+            return jwt;
+        }
 
-		public async Task<ServiceResponse<bool>> ChangePassword(int userId, string newPassword)
-		{
-			var user = await _context.Users.FindAsync(userId);
-			if (user == null)
-			{
-				return new ServiceResponse<bool>
-				{
-					Success = false,
-					Message = "User not found."
-				};
-			}
+        public async Task<ServiceResponse<bool>> ChangePassword(int userId, string newPassword)
+        {
+            var user = await _context.Users.FindAsync(userId);
+            if (user == null)
+            {
+                return new ServiceResponse<bool>
+                {
+                    Success = false,
+                    Message = "User not found."
+                };
+            }
 
-			CreatePasswordHash(newPassword, out byte[] passwordHash, out byte[] passwordSalt);
+            CreatePasswordHash(newPassword, out byte[] passwordHash, out byte[] passwordSalt);
 
-			user.PasswordHash = passwordHash;
-			user.PasswordSalt = passwordSalt;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
 
-			await _context.SaveChangesAsync();
+            await _context.SaveChangesAsync();
 
-			return new ServiceResponse<bool> { Data = true, Message = "Password has been changed." };
-		}
-	}
+            return new ServiceResponse<bool> { Data = true, Message = "Password has been changed." };
+        }
+    }
 }
