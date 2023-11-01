dd54bc751daa9a14d9dbd7ad17516a47851cc7e3
ziuluiz <luiz_henrique_silva@outlook.com>
2023-11-01 05:27:49 -0300

    some fixes and refactoring

--- InvestmentPlataform/Client/Services/AuthServices/IAuthService.cs
+++ InvestmentPlataform/Client/Services/AuthServices/IAuthService.cs
@@ -5,6 +5,5 @@
 		Task<ServiceResponse<int>> Register(UserRegister request);
 		Task<ServiceResponse<string>> Login(UserLogin request);
 		Task<ServiceResponse<bool>> ChangePassword(UserChangePassword request);
-
 	}
 }
